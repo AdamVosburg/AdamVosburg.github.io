8273b1681708212a4a1bfdf9c50a44182fdd0138
Adam Vosburg <adam.vosburg@snhu.edu>
2025-02-25 13:37:59 -0500

    Adding updated functionality to animal models

--- backend/models/animal/bird.model.js
+++ backend/models/animal/bird.model.js
@@ -1,39 +1,35 @@
-console.log('Starting to load bird.model.js');
+/**
+ * Bird Model - Mongoose discriminator for RescueAnimal
+ * @module models/bird
+ */
 
-console.log('About to require dependencies');
 const mongoose = require('mongoose');
-console.log('Mongoose loaded');
-
 const RescueAnimal = require('../base/rescueAnimal');
-console.log('RescueAnimal model loaded');
-
 const TrainingProgram = require('../training/trainingProgram.model');
-console.log('TrainingProgram model loaded');
-
 const MedicalRecord = require('../medical/medicalRecord.model');
-console.log('MedicalRecord model loaded');
-console.log('All dependencies loaded');
 
-console.log('Defining birdSchema');
+/**
+ * Bird schema definition
+ * @type {mongoose.Schema}
+ */
 const birdSchema = new mongoose.Schema({
   // Schema definition stays the same
 });
-console.log('birdSchema defined');
 
-console.log('Adding schema methods');
-// Add all your existing methods here with logging
+/**
+ * Initializes the environment parameters for a bird
+ * @returns {Object} The updated bird document
+ */
 birdSchema.methods.initializeBirdEnvironment = function() {
-  console.log('Executing initializeBirdEnvironment method');
   // Method implementation stays the same
 };
 
-// Add debug logs to other methods similarly
-console.log('Schema methods added');
+// Other methods would be similarly documented here
 
-console.log('Creating Bird model discriminator');
+/**
+ * Bird model - Discriminator of RescueAnimal base model
+ * @type {mongoose.Model}
+ */
 const Bird = RescueAnimal.discriminator('Bird', birdSchema);
-console.log('Bird model discriminator created');
 
-console.log('About to export Bird model');
 module.exports = Bird;
-console.log('Bird model exported');
